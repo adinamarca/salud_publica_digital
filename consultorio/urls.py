dad9532f423aa0924e7dd8346e6a7507cb6ad7ee
Alejandro Dinamarca <a.dinamarca@canalcero.com>
2024-09-26 22:53:25 -0300

    feat: shorter api route name

--- consultorio/urls.py
+++ consultorio/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", views.seleccionar_region, name="consultorio"),
     path("mis_horas", views.mis_horas, name="mis_horas"),
     path("cancelar_hora", views.cancelar_hora, name="cancelar_hora"),
-    path("consultorio/obtener_comunas/<int:c_reg>/", views.obtener_comunas, name="obtener_comunas"),
-    path("consultorio/obtener_consultorios/<str:c_com>/", views.obtener_consultorios, name="obtener_consultorios"),
+    path("obtener_comunas/<int:c_reg>/", views.obtener_comunas, name="obtener_comunas"),
+    path("obtener_consultorios/<str:c_com>/", views.obtener_consultorios, name="obtener_consultorios"),
 ]
